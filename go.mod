ee0bc3ea86cc6276229fc086c9ad92f9b4f1b1e8
damian3197 <damiansitanggang@gmail.com>
2022-03-13 15:16:33 +0700

    add function

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/damian3197/tugasgomodulesmain
 
 go 1.17
 
-require github.com/damian3197/tugasgomodules v1.0.3
+require github.com/damian3197/tugasgomodules v1.0.4
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	modul "github.com/damian3197/tugasgomodules" 
+	"fmt"
+
+	modul "github.com/damian3197/tugasgomodules"
 )
 
 func main(){
-
+	I := 5
+	V := 120
 	modul.Welcome()
 	modul.Verifikasi("Damian", "Sitanggang")
+	modul.AnonymousDataBuku()
+	var power, resistor = modul.PowerCal(I, V)
+	fmt.Println("Arus =", I, " Tegangan = ", V, " Maka Daya = ", power, " Tahanan = ", resistor)
 }
