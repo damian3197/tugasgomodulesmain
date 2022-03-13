bf9e36865e5f42533240c81869e18be97e8ec30a
damian3197 <damiansitanggang@gmail.com>
2022-03-13 14:59:37 +0700

    initialize

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/damian3197/tugasgomodulesmain
+
+go 1.17
+
+require github.com/damian3197/tugasgomodules v1.0.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	modul "github.com/damian3197/tugasgomodules" 
+)
+
+func main(){
+
+	modul.Welcome()
+	modul.Verifikasi("Damian", "Sitanggang")
+}
